e600a6020ac8ea68f8086a722a7bdec41a153483
Teo Georgiev <t.georgiev.g@gmail.com>
2023-06-11 17:00:10 +0300

    Split the processing of the files into functions.

--- mad-libs.py
+++ mad-libs.py
@@ -1,65 +1,70 @@
-# Mad Libs exercise 
-
-# Read story from file
-# Separate missing parts from static parts
-# Ask for appropriate work / phrase 
-# Add that to missing part 
-# Update text 
-# Write in file 
-
+# MAD LIBS EXERCISE
 import sys
 
 class StoryFull: 
     def __init__(self):
         self.__story = []
-
+        
+    def execute(self): 
+        self.read_file()
+        self.substitute_words()
+        self.write_file()
+        print(self.get_story())
+        
+    # Read the source story file
     def read_file(self): 
-        with open("story.txt") as f, open("new_story.txt", "w") as new:
-            full_story = f.read()
-            story_list = full_story.split(" ")
-            
-            self.substitute_words(story_list)
-            
-            print(self.get_story())
-            new.write(self.get_story())
-            new.close()
+        with open("story.txt") as f:
+            self.__story = f.read().split(" ")
     
-    def substitute_words(self, story: list):
-         for word in story: 
-                if word.find(")") != -1: 
-                    # Getting the prompt word from the string
-                    prompt = word[word.find("(") + 1 : word.find(")")] 
-                    word = self.prompt(prompt)
-                    if word == "0":
-                        return
-                    print("+ Word added!")
-                    print()
-                self.__story.append(word)
-    
+    # Write the new story in a separate file
+    def write_file(self):
+        with open("new_story.txt", "w") as new_f:
+            new_f.write(self.get_story())
+            new_f.close()
+        
     # Return current status of story. 
     def get_story(self):
-        return " ".join(self.__story)
+        if self.substitute_words() == "0":
+            print("Closing the game. Bye!")
+            return None
+        else: 
+            return " ".join(self.__story)
+    
+    def substitute_words(self):
+        for i in range(len(self.__story)): 
+            word = self.__story[i]
+            if word.find("(") != -1: 
+                # Getting the prompt word from the string
+                prompt = word[word.find("(") + 1 : word.find(")")] 
+                new_word = self.prompt(prompt) + word[word.find(")") + 1:]
+                
+                # Check if the user wanted to quit the fame and if not, append the word to the sentence
+                if new_word == "0":
+                    return "0"
+                self.__story[i] = new_word
+                print("+ Word added!")
+                print()
+        return self.__story
     
     # Function for providing the response to the prompt
     def prompt(self, prompt): 
         while True: 
-            if prompt[0] in "aeoui":
+            # Need to check whether to use A or AN
+            if prompt[0] in "aeoui": 
                 print(f"Give me an {prompt.upper()}: ")
             else: 
                 print(f"Give me a {prompt.upper()}: ")
             
+            # PROMPT input
             print(f"{prompt.upper()} >")
             response = sys.stdin.readline().strip()
             
+            # PROMPT test
             if response.isspace(): 
                 print("You must enter a word.")
                 continue
-            elif response == "0": 
-                print("Closing the game. Bye!")
-                return "0"
-            else: 
-                break
-        
+            else: break
+            
         return response
 
 class MadLibsApplication: 
@@ -74,7 +79,7 @@ class MadLibsApplication:
     
     def execute(self):
         self.help()
-        self.__story.read_file()
+        self.__story.execute()
         
 application = MadLibsApplication()
 application.execute()
